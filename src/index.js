abd8df2ba9665071efcb7589335d0ec2b04c6ebd
Ori Marron <orim@wix.com>
2021-04-18 16:49:07 +0300

    commit 1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import "./styles.css";
 
 const choices = document.querySelectorAll("[aria-keyshortcuts]");
 
-const handleKeyDown = event => {
+const handleKeyDown = (event) => {
   const keyCode = event.keyCode;
   const character = String.fromCharCode(keyCode);
   const choice = [...choices].find(
-    c => c.getAttribute("aria-keyshortcuts") === character.toLowerCase()
+    (c) => c.getAttribute("aria-keyshortcuts") === character.toLowerCase()
   );
   // TODO: Select "choice"
   if (choice) {
     console.log(choice);
+    // dsfgsdgdfs
+    // sdgdsg
+    // asdfsafda
   }
 };
 
